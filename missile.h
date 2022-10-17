3e0395bd1634fb9c8a70ad2c6f0a4fb5f272613c
Jack Barton <jjb106@cs18272kq.canterbury.ac.nz>
2022-10-18 11:34:09 +1300

    comment improved

--- missile.h
+++ missile.h
@@ -14,23 +14,31 @@
 #include "ir_uart.h"
 
 
-/* Creates a missile */
+/** Creates a missile
+    @return a missile object type */
 game_object_t missile_create(void);
 
 
-/* Launches the missile */
+/** Launches the missile
+    @param pointer of the missile
+    @param position of the missile */
 void missile_launch(game_object_t* missile_ptr, tinygl_point_t pos);
 
 
-/* Sends a missile to other funkit */
+/** Sends a missile to other funkit
+    uses flipped_y to account for the funkit orientation
+    @param y-coordinate of the missile */
 void missile_send(tinygl_coord_t y);
 
 
-/* Updates the missile  */
+/** Updates the missile 
+    @param Missile pointer */
 void missile_update(game_object_t* missile_ptr);
 
 
-/* Gets the position of a game object through coordinates */
+/** Gets the position of a game object through coordinates
+    @param game object
+    @return a point */
 void incoming_missile_update(game_object_t* incoming_missile_ptr);
 
 
